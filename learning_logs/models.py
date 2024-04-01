0f79f79aa3e8a5a30ab731e437cdf45770ddd3cd
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-04-01 13:50:37 +0100

    working with user data (topics and info)

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -5,6 +5,15 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+class Entry(models.Model):
+    """ more text field for other things about a topic """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
     def __str__(self):
         """ Return the string representation of the created model"""
-        return self.text
+        return f"{self.text[:50]}..."
